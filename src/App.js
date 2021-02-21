13e086a1be4cb6d15599b19c012635314c382bd9
Patryk Mysiorski <mysior@ncdc.pl>
2021-02-21 20:27:20 +0100

    create simple chat

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom"
+import Home from "./Components/Home"
+import ChatRoom from "./Components/ChatRoom";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () =>
+    <BrowserRouter>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route exact path="/:roomId" component={ChatRoom} />
+        </Switch>
+    </BrowserRouter>
 
 export default App;
